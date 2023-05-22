4382238bba502edeed5e7136bbc300c4d827d0be
dinhphamcanh <dinhpc@solashi.com>
2023-05-22 23:42:16 +0700

    feat:responsive footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,4 @@
 import { Box, Container, Typography } from "@mui/material";
-import Image from "next/image";
-import Logo from "../../../public/img/png/logo.png";
 import {
   MapPinIcon,
   PhoneIcon,
@@ -16,88 +14,106 @@ const Footer = () => {
           <Box
             display={"flex"}
             justifyContent={"space-between"}
-            alignItems={"center"}
+            gap={8}
+            sx={{
+              flexDirection: { xs: "column", md: "row" },
+              alignItems: { xs: "flex-start", lg: "center" },
+            }}
           >
-            <Box>
-              <Image src={Logo} alt="logo" priority={true} width={300} />
-            </Box>
-            <Box>
-              <Typography variant="h5" fontWeight={700} color="primary">
-                WEBSITE BÁN TRANH CHÍNH THỨC CỦA MỸ THUẬT ĐÔNG ANH
-              </Typography>
+            <Box width={200}>
               <Box
-                mt={5}
-                display={"flex"}
-                flexDirection={"column"}
-                gap={2}
-                sx={{ cursor: "pointer" }}
-              >
-                <Box display={"flex"} alignItems={"center"} gap={1.5}>
-                  <MapPinIcon width={18} />
-                  <Typography fontWeight={500} variant="h5" color="primary">
-                    Địa chỉ: Đại Mạch, Đông Anh, Hà Nội.
-                  </Typography>
+                component="img"
+                src={"/img/png/logo.png"}
+                alt="logo"
+                width={"100%"}
+              />
+            </Box>
+            <Box
+              display={"flex"}
+              justifyContent={"space-between"}
+              gap={8}
+              sx={{
+                flexDirection: { xs: "column", sm: "row" },
+              }}
+            >
+              <Box>
+                <Typography variant="h5" fontWeight={700} color="primary">
+                  WEBSITE BÁN TRANH CHÍNH THỨC CỦA MỸ THUẬT ĐÔNG ANH
+                </Typography>
+                <Box
+                  mt={5}
+                  display={"flex"}
+                  flexDirection={"column"}
+                  gap={2}
+                  sx={{ cursor: "pointer" }}
+                >
+                  <Box display={"flex"} alignItems={"center"} gap={1.5}>
+                    <MapPinIcon width={18} />
+                    <Typography fontWeight={500} variant="h5" color="primary">
+                      Địa chỉ: Đại Mạch, Đông Anh, Hà Nội.
+                    </Typography>
+                  </Box>
+                  <Box display={"flex"} alignItems={"center"} gap={1.5}>
+                    <PhoneIcon width={18} />
+                    <Typography fontWeight={500} variant="h5" color="primary">
+                      Chăm sóc khách hàng: 0975.146.588
+                    </Typography>
+                  </Box>
+                  <Box display={"flex"} alignItems={"center"} gap={1.5}>
+                    <EnvelopeIcon width={18} />
+                    <Typography fontWeight={500} variant="h5" color="primary">
+                      Email: mythuatdonganh@gmail.com
+                    </Typography>
+                  </Box>
                 </Box>
-                <Box display={"flex"} alignItems={"center"} gap={1.5}>
-                  <PhoneIcon width={18} />
-                  <Typography fontWeight={500} variant="h5" color="primary">
-                    Chăm sóc khách hàng: 0975.146.588
-                  </Typography>
-                </Box>
-                <Box display={"flex"} alignItems={"center"} gap={1.5}>
-                  <EnvelopeIcon width={18} />
-                  <Typography fontWeight={500} variant="h5" color="primary">
-                    Email: mythuatdonganh@gmail.com
-                  </Typography>
+                <Box mt={4}>
+                  <ListSocial />
                 </Box>
               </Box>
-              <Box mt={4}>
-                <ListSocial />
-              </Box>
-            </Box>
-            <Box>
-              <Typography variant="h5" fontWeight={700} color="primary">
-                Giới thiệu & Dịch vụ
-              </Typography>
-              <Box
-                mt={3}
-                display={"flex"}
-                flexDirection={"column"}
-                gap={2}
-                sx={{ cursor: "pointer" }}
-              >
-                <Typography
-                  fontWeight={500}
-                  variant="h5"
-                  mt={1.5}
-                  color="primary"
-                >
-                  Tin tức công ty
+              <Box>
+                <Typography variant="h5" fontWeight={700} color="primary">
+                  Giới thiệu & Dịch vụ
                 </Typography>
-                <Typography
-                  fontWeight={500}
-                  variant="h5"
-                  mt={1.5}
-                  color="primary"
+                <Box
+                  mt={3}
+                  display={"flex"}
+                  flexDirection={"column"}
+                  gap={2}
+                  sx={{ cursor: "pointer" }}
                 >
-                  Tuyển dụng
-                </Typography>
-                <Typography
-                  fontWeight={500}
-                  variant="h5"
-                  mt={1.5}
-                  color="primary"
-                >
-                  Dịch vụ bảo hành
-                </Typography>
-                <Typography
-                  fontWeight={500}
-                  variant="h5"
-                  mt={1.5}
-                  color="primary"
-                >
-                  Dịch vụ sửa chữa
-                </Typography>
+                  <Typography
+                    fontWeight={500}
+                    variant="h5"
+                    mt={1.5}
+                    color="primary"
+                  >
+                    Tin tức công ty
+                  </Typography>
+                  <Typography
+                    fontWeight={500}
+                    variant="h5"
+                    mt={1.5}
+                    color="primary"
+                  >
+                    Tuyển dụng
+                  </Typography>
+                  <Typography
+                    fontWeight={500}
+                    variant="h5"
+                    mt={1.5}
+                    color="primary"
+                  >
+                    Dịch vụ bảo hành
+                  </Typography>
+                  <Typography
+                    fontWeight={500}
+                    variant="h5"
+                    mt={1.5}
+                    color="primary"
+                  >
+                    Dịch vụ sửa chữa
+                  </Typography>
+                </Box>
               </Box>
             </Box>
           </Box>
