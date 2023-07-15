5d3bd7ac779a874b20a0d214a722f850e7e46f43
dinhphamcanh <dinhpc@solashi.com>
2023-07-15 16:49:44 +0700

    fix: test deploy

--- src/lib/context/AuthContext.tsx
+++ src/lib/context/AuthContext.tsx
@@ -56,6 +56,7 @@ const reducer = (state: AuthType, action: Action): AuthType => {
     }
 
     case "LOGIN": {
+      console.log("hello");
       const { accessToken, user, refreshToken } = action.payload;
       return {
         ...state,
