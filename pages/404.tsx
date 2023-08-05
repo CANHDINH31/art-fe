72bfdd0600cce11cd2ad2d70b48b220050b78834
dinhphamcanh <dinhpc@solashi.com>
2023-08-05 17:09:21 +0700

    feat/error-page

---
+++ pages/404.tsx
@@ -0,0 +1,15 @@
+import { Box, Button } from "@mui/material";
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <Box
+      component={"img"}
+      src={"/img/svg/ErrorPage.svg"}
+      width={"100vw"}
+      height={"100vh"}
+    />
+  );
+};
+
+export default NotFoundPage;
